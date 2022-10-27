c9c327bed62c3c2b02d8db3e98d395ce297cf758
emon21 <emonbd66@gmail.com>
2022-10-27 18:43:51 +0600

    testing

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,23 @@
 module.exports = {
     content: ["./src/**/*.{html,js}","./**/*.{html,js}","index.html"],
   theme: {
-    extend: {},
+    extend: {
+        fontFamily:{
+            // poppins:['poppins','sans-serif'],
+            'public_sans':['Public Sans', 'sans-serif']
+        },
+        container:{
+            center:true,
+            padding:'0px',
+            screens:{
+                'sm': "512px",
+                'md': "720px",
+                'lg': "1024px",
+                'xl': "1140px",
+                '2xl': "1320px"
+            }
+        },
+    },
   },
   plugins: [],
 }
